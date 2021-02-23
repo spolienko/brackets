f83cf1081790af6016d1fa6519003c998c545744
Sofiya Polienko <sofie.polienko@gmail.com>
2021-02-23 13:05:48 +0500

    feat: 13/20 tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = bracketsConfig.flat();
+    const openBraket = brackets.filter((e, i) => !(i % 2));
+    const closeBraket = brackets.filter((e, i) => !(i % 2 == 0));
+    let flag = false;
+    let bracketsIndex = [],
+        closeIndex,
+        openIndex;
+
+    for (let i = 0; i < (str.length); i++) {
+        openIndex = openBraket.indexOf(str[i]);
+        if (openIndex !== -1) {
+            if (str[i] === '|' && flag == false) {
+                flag == true;
+                bracketsIndex.push(openIndex);
+            }
+            else {
+                bracketsIndex.push(openIndex);
+            }
+            continue;
+        }
+        closeIndex = closeBraket.indexOf(str[i]);
+        if (closeIndex !== -1) {
+            if (str[i] === '|' && flag == true) {
+                flag == false;
+                openIndex = bracketsIndex.pop();
+            }
+            else {
+                openIndex = bracketsIndex.pop();
+            }
+            if (openIndex != closeIndex) {
+                return false
+            }
+        }
+    }
+    if (bracketsIndex.length !== 0) {
+        return false;
+    }
+    return true;
 }
