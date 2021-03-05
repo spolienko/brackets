6d462ac52a782c5d3d523f677b1459a7f2266045
Sofiya Polienko <sofie.polienko@gmail.com>
2021-03-05 21:43:49 +0500

    feat: add solution with passing tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const openers = bracketsConfig.map(x => x[0]);
+    const closers = bracketsConfig.map(x => x[1]);
+    const stack = [];
+    for (let chr of str) {
+        let closerIndex = closers.findIndex(el => el === chr);
+        let openerIndex = openers.findIndex(el => el === chr);
+        if (closerIndex === openerIndex) {
+            if (stack.filter(x => x == chr).length % 2 !== 0) { 
+                openerIndex = -1;
+            }
+        }
+        if (openerIndex !== -1) {
+            stack.push(chr);
+        } else {
+            let lastOpener = stack.pop();
+            let trueOpener = openers[closerIndex];
+            if (lastOpener !== trueOpener) return false;
+        }
+    }
+    return stack.length === 0;
 }
